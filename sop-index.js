2e87990a73f96dc8812376b7f7f04dd6eb682d54
marbleartemisa <artemisamarblesystem@gmail.com>
2026-03-06 09:41:13 -0500

    Add files via upload

---
+++ sop-index.js
@@ -0,0 +1,45 @@
+const SOP_INDEX = [
+
+{
+title:"Cotización de Ventas",
+department:"Comercial",
+keywords:"cotizacion ventas presupuesto cliente",
+url:"/sop-site/comercial/SOP PARA COTIZACIÓN DE VENTAS rev 1-.html"
+},
+
+{
+title:"Sales Consultant",
+department:"Comercial",
+keywords:"sales consultant ventas asesor cliente",
+url:"/sop-site/comercial/SOP SALES CONSULTANT1.html"
+},
+
+{
+title:"Mantenimiento Preventivo Trimming",
+department:"Maquinarias",
+keywords:"trimming mantenimiento maquina",
+url:"/sop-site/maquinarias/SOP-MANTENIMIENTO-PREVENTIVO-TRIMMING.html"
+},
+
+{
+title:"Calibración Contadores Edgebander",
+department:"Maquinarias",
+keywords:"edgebander edgebanding calibracion borde pvc",
+url:"/sop-site/maquinarias/SOP – Calibración y Uso de Contadores Mecánicos del Edgebander Final.html"
+},
+
+{
+title:"Calibrar Edgebanding",
+department:"Maquinarias",
+keywords:"edgebanding canteadora calibracion borde",
+url:"/sop-site/maquinarias/SOP –CALIBRAR-EDGEBANDING.html"
+},
+
+{
+title:"Conducción de Vehículos",
+department:"Vehículos",
+keywords:"vehiculo camion manejo transporte",
+url:"/sop-site/vehiculos/SOP-CONDUCCION-VEHICULOS.html"
+}
+
+]
