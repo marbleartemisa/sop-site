d662b7c0edf98f910e50ed5ab3923044ffa25454
marbleartemisa <artemisamarblesystem@gmail.com>
2026-06-05 14:35:51 -0400

    Update estimator.js

--- js/estimator.js
+++ js/estimator.js
@@ -1,1 +1,31 @@
+function estimateProject(project) {
 
+  let baseFt2 = Number(project.Ft2 || 0);
+
+  let materialFactor = getMaterialFactor(project.Material);
+  let complexityFactor = getComplexity(project);
+  let pieceFactor = getPieceFactor(project);
+
+  // CUT
+  let cutRate = getCutRate(project.CutMethod);
+
+  let cutTime =
+    baseFt2 *
+    cutRate *
+    materialFactor *
+    complexityFactor *
+    pieceFactor;
+
+  // FAB
+  let fabTime =
+    baseFt2 *
+    0.45 *
+    materialFactor *
+    complexityFactor;
+
+  return {
+    cutTime,
+    fabTime,
+    total: cutTime + fabTime
+  };
+}
