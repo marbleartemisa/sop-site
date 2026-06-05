49da885767528b1594931ca9db6a44b2139861cf
marbleartemisa <artemisamarblesystem@gmail.com>
2026-06-05 09:26:18 -0400

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,13 @@
+function loadData() {
+  fetch(API_URL)
+    .then(r => r.json())
+    .then(data => {
+      renderDashboard(data);
+    });
+}
 
+function renderDashboard(data) {
+  // breton queue
+  // manual queue
+  // all schedule
+}
