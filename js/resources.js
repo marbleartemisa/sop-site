47229dc1086565089ff98411e8f7f08fa7614401
marbleartemisa <artemisamarblesystem@gmail.com>
2026-06-05 11:17:42 -0400

    Add renderResources function to display resource load

--- js/resources.js
+++ js/resources.js
@@ -1,1 +1,21 @@
+function renderResources() {
 
+  const container = document.getElementById("view-container");
+
+  let html = `<div class="panel"><h2>⚙️ Resource Load</h2>`;
+
+  const load = {};
+
+  STATE.schedule.forEach(s => {
+    if (!load[s.Resource]) load[s.Resource] = 0;
+    load[s.Resource] += Number(s.PF);
+  });
+
+  Object.keys(load).forEach(r => {
+    html += `<p>${r}: ${load[r]} PF</p>`;
+  });
+
+  html += "</div>";
+
+  container.innerHTML = html;
+}
