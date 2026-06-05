ba525f968646cfb1a9d18fa37ad7c4fdd98f466c
marbleartemisa <artemisamarblesystem@gmail.com>
2026-06-05 11:14:22 -0400

    Add fetchSchedule function to retrieve schedule data

--- js/api.js
+++ js/api.js
@@ -1,1 +1,6 @@
+const API = "https://script.google.com/macros/s/AKfycbzVLUTOzA0gGIfKml7kWBAWIjSE6g473aBDmFCg-cN2UzSG2-VnKElingOTgCSdeIumfg/exec";
 
+async function fetchSchedule() {
+  const res = await fetch(API);
+  return await res.json();
+}
