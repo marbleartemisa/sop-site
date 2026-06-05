d742b7c09135ba708fe345e5549499d091505745
marbleartemisa <artemisamarblesystem@gmail.com>
2026-06-05 11:15:10 -0400

    Update scheduler.js

--- js/scheduler.js
+++ js/scheduler.js
@@ -1,1 +1,19 @@
+async function initERP() {
+  await refresh();
+  showView('projects');
+}
 
+async function refresh() {
+  STATE.schedule = await fetchSchedule();
+  renderProjects();
+}
+
+function showView(view) {
+  const container = document.getElementById("view-container");
+
+  if (view === "projects") renderProjects();
+  if (view === "gantt") renderGantt();
+  if (view === "resources") renderResources();
+  if (view === "create") renderCreateProject();
+  if (view === "history") renderHistory();
+}
