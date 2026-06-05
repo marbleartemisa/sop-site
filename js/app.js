52c13b0ef9ee1d77188d13a2e132854e26842605
marbleartemisa <artemisamarblesystem@gmail.com>
2026-06-05 16:10:07 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,36 @@
+import { generateSchedule } from "./scheduler.js";
+import { state } from "./state.js";
 
+window.app = {
+  init,
+  view: "projects"
+};
+
+async function init() {
+  console.log("ERP INIT");
+
+  await loadInitialData();
+
+  showView("projects");
+}
+
+async function loadInitialData() {
+  // aquí luego conectas API real
+  console.log("Loading data...");
+}
+
+window.showView = function (view) {
+  const container = document.getElementById("view-container");
+
+  if (view === "projects") {
+    container.innerHTML = renderProjects();
+  }
+
+  if (view === "gantt") {
+    container.innerHTML = renderGantt();
+  }
+
+  if (view === "create") {
+    container.innerHTML = renderProjectConfigurator();
+  }
+};
