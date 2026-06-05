73dccaf79b44db601c6b92da9ff08bdadf77deaa
marbleartemisa <artemisamarblesystem@gmail.com>
2026-06-05 11:33:51 -0400

    Add POST and GET functions for API interactions

--- js/api.js
+++ js/api.js
@@ -1,5 +1,21 @@
 const API = "https://script.google.com/macros/s/AKfycbzVLUTOzA0gGIfKml7kWBAWIjSE6g473aBDmFCg-cN2UzSG2-VnKElingOTgCSdeIumfg/exec";
 
+async function post(action, data = {}) {
+
+  const res = await fetch(API, {
+    method: "POST",
+    body: JSON.stringify({ action, data }),
+    headers: { "Content-Type": "application/json" }
+  });
+
+  return await res.json();
+}
+
+async function getProjects() {
+  const res = await fetch(API + "?action=projects");
+  return await res.json();
+}
+
 async function fetchSchedule() {
   const res = await fetch(API);
   return await res.json();
