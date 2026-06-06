4546be2e04e5b9a29e38c624b22db29a5590c98b
marbleartemisa <artemisamarblesystem@gmail.com>
2026-06-06 13:16:47 -0400

    Add groupBy and formatDate utility functions

--- utils/schedule.js
+++ utils/schedule.js
@@ -1,1 +1,11 @@
+export function groupBy(data, key) {
+  return data.reduce((acc, item) => {
+    if (!acc[item[key]]) acc[item[key]] = [];
+    acc[item[key]].push(item);
+    return acc;
+  }, {});
+}
 
+export function formatDate(date) {
+  return new Date(date).toLocaleDateString();
+}
