e43952798466311190a780472dbc27b7a45928c8
marbleartemisa <artemisamarblesystem@gmail.com>
2026-06-10 20:12:00 -0400

    Update timeEngine.js

--- js/timeEngine.js
+++ js/timeEngine.js
@@ -1,1 +1,26 @@
+export const TIME_RATES = {
 
+  CARPENTRY: {
+    cncCut: 8,              // min per panel
+    edgeBand: 0.5,          // min per linear ft
+    cabinet: 10,
+    drawer: 20,
+    pantry: 20,
+    trashcan: 25,
+    lazySusan: 25,
+    lemans: 25,
+    pocketPantry: 90,
+    pocketCabinet: 60,
+    extraCut: 10
+  },
+
+  STONE: {
+    thicknessFactor: {
+      "6mm": 0.8,
+      "8mm": 1.0,
+      "12mm": 1.2,
+      "2cm": 1.5,
+      "3cm": 2.0
+    }
+  }
+};
