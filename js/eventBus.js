a65f33314e13a4a79a263d870f3bd83a58f9e27f
marbleartemisa <artemisamarblesystem@gmail.com>
2026-06-10 21:28:30 -0400

    Update eventBus.js

--- js/eventBus.js
+++ js/eventBus.js
@@ -1,1 +1,12 @@
+export const EventBus = {
+  events: {},
 
+  on(event, fn) {
+    if (!this.events[event]) this.events[event] = [];
+    this.events[event].push(fn);
+  },
+
+  emit(event, payload) {
+    (this.events[event] || []).forEach(fn => fn(payload));
+  }
+};
