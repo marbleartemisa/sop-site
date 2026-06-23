b630d6e881f45c81b12a340562ba68054e974fe2
marbleartemisa <artemisamarblesystem@gmail.com>
2026-06-23 13:03:47 -0400

    Add STONE configuration with setup and edge types

--- dashboard/js/data/stone-times.js
+++ dashboard/js/data/stone-times.js
@@ -1,1 +1,17 @@
+export const STONE = {
 
+    BRETON_SETUP:25,
+
+    COCH_SETUP:30,
+
+    EDGE_TYPES:{
+
+        Eased:2,
+        Pencil:3,
+        HalfBullnose:4,
+        FullBullnose:5,
+        Ogee:8,
+        Miter:10,
+        Laminated:12
+    }
+};
