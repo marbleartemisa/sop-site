f7c78dbf6b27a0a1f4f222ce75fe6af5de2a5f8c
marbleartemisa <artemisamarblesystem@gmail.com>
2026-06-23 11:18:15 -0400

    Update carpentry.js

--- dashboard/js/calculators/carpentry.js
+++ dashboard/js/calculators/carpentry.js
@@ -1,1 +1,36 @@
+export function calculateCarpentry(data){
 
+  const cnc =
+      data.panels * 9;
+
+  const edge =
+      data.edgeLF * 0.60;
+
+  const cabinets =
+      data.cabinets * 10;
+
+  const drawers =
+      data.drawers * 20;
+
+  const pantry =
+      data.pantry * 20;
+
+  const total =
+      cnc +
+      edge +
+      cabinets +
+      drawers +
+      pantry;
+
+  return {
+
+    cnc,
+    edge,
+    cabinets,
+    drawers,
+    pantry,
+    total
+
+  };
+
+}
