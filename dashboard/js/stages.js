e0697d9862ecb51f26131d3c6b14eeb8cef50cfb
marbleartemisa <artemisamarblesystem@gmail.com>
2026-06-23 12:59:08 -0400

    Update stages.js

--- dashboard/js/stages.js
+++ dashboard/js/stages.js
@@ -2,75 +2,23 @@
 
 export const stages = [
 
-    {
-        id:1,
-        name:"Agreement",
-        days:0
-    },
+    {id:1,name:'Agreement',days:0},
+    {id:2,name:'Measure Confirmation',days:3},
+    {id:3,name:'Scheduling',days:3},
+    {id:4,name:'Material Order',days:4},
+    {id:5,name:'Final Approval',days:3},
 
-    {
-        id:2,
-        name:"Measure Confirmation",
-        days:3
-    },
+    {id:6,name:'Carpentry Fabrication',days:0},
 
-    {
-        id:3,
-        name:"Scheduling",
-        days:3
-    },
+    {id:7,name:'Carpentry Installation',days:2.5},
 
-    {
-        id:4,
-        name:"Material Order",
-        days:4
-    },
+    {id:8,name:'Stone Measure',days:2},
 
-    {
-        id:5,
-        name:"Final Approval",
-        days:3
-    },
+    {id:9,name:'Stone Approval',days:3},
 
-    {
-        id:6,
-        name:"Carpentry Fabrication",
-        days:0
-    },
+    {id:10,name:'Stone Fabrication',days:0},
 
-    {
-        id:7,
-        name:"Carpentry Installation",
-        days:2.5
-    },
+    {id:11,name:'Stone Installation',days:3},
 
-    {
-        id:8,
-        name:"Stone Measure",
-        days:2
-    },
-
-    {
-        id:9,
-        name:"Stone Approval",
-        days:3
-    },
-
-    {
-        id:10,
-        name:"Stone Fabrication",
-        days:0
-    },
-
-    {
-        id:11,
-        name:"Stone Installation",
-        days:3
-    },
-
-    {
-        id:12,
-        name:"Punchout",
-        days:2
-    }
+    {id:12,name:'Punchout',days:2}
 ];
