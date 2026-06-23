ac6d1ad480c7cc3b3deb3b6b9e97b77e68bf7e8b
marbleartemisa <artemisamarblesystem@gmail.com>
2026-06-23 11:16:42 -0400

    Add functions to open modal and log actions

--- dashboard/js/app.js
+++ dashboard/js/app.js
@@ -1,1 +1,13 @@
+import { openProjectModal } from "./project-modal.js";
 
+window.openCreateForm = () => {
+  openProjectModal();
+};
+
+window.refreshData = () => {
+  console.log("refresh");
+};
+
+window.runScheduler = () => {
+  console.log("scheduler");
+};
