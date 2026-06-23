a83409aa91f019f6f74ba5c8bbb68e41e3ceec6b
marbleartemisa <artemisamarblesystem@gmail.com>
2026-06-23 16:19:11 -0400

    Update state-engine.js

--- dashboard/js/state-engine.js
+++ dashboard/js/state-engine.js
@@ -1,1 +1,39 @@
+export function createStore(initialState) {
 
+    let state = initialState;
+    let listeners = [];
+
+    function getState() {
+        return state;
+    }
+
+    function setState(partial) {
+        state = {
+            ...state,
+            ...partial
+        };
+
+        notify();
+    }
+
+    function subscribe(fn) {
+        listeners.push(fn);
+
+        // inicial run
+        fn(state);
+
+        return () => {
+            listeners = listeners.filter(l => l !== fn);
+        };
+    }
+
+    function notify() {
+        listeners.forEach(fn => fn(state));
+    }
+
+    return {
+        getState,
+        setState,
+        subscribe
+    };
+}
