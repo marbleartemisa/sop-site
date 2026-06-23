041f0672bae42040df255475ae1496556a00e16f
marbleartemisa <artemisamarblesystem@gmail.com>
2026-06-23 11:18:38 -0400

    Add calculateStone function for stone calculations

--- dashboard/js/calculators/stone.js
+++ dashboard/js/calculators/stone.js
@@ -1,1 +1,26 @@
+export function calculateStone(data){
 
+  const cut =
+      data.slabs * 25;
+
+  const edge =
+      data.edgeLF * 2;
+
+  const cutouts =
+      data.cutouts * 15;
+
+  const total =
+      cut +
+      edge +
+      cutouts;
+
+  return {
+
+    cut,
+    edge,
+    cutouts,
+    total
+
+  };
+
+}
