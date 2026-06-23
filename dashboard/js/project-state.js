f06a881d82544cb02a134296687010350a4d37c2
marbleartemisa <artemisamarblesystem@gmail.com>
2026-06-23 11:17:37 -0400

    Update project-state.js

--- dashboard/js/project-state.js
+++ dashboard/js/project-state.js
@@ -1,1 +1,46 @@
+export const projectState = {
 
+  projectName: "",
+
+  stages: {
+
+    carpentry:true,
+    stone:true
+  },
+
+  carpentry: {
+
+    complexity:3,
+
+    panels:0,
+    cabinets:0,
+    drawers:0,
+    pantry:0,
+
+    trashcan:0,
+    lazySusan:0,
+    lemans:0,
+
+    edgeLF:0
+  },
+
+  stone: {
+
+    machine:"Breton",
+
+    material:"Quartz",
+
+    thickness:20,
+
+    sqft:0,
+
+    slabs:0,
+
+    pieces:0,
+
+    edgeLF:0,
+
+    cutouts:0
+  }
+
+};
