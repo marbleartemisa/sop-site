77447e19a08d15679c578f3548b53612603f4e89
marbleartemisa <artemisamarblesystem@gmail.com>
2026-06-27 12:46:00 -0400

    Add fetchSchedule function to retrieve schedule data

--- dashboard/js/services/schedule-service.js
+++ dashboard/js/services/schedule-service.js
@@ -1,1 +1,22 @@
+const BACKEND_URL =
+  "https://script.google.com/macros/s/AKfycbzVLUTOzA0gGIfKml7kWBAWIjSE6g473aBDmFCg-cN2UzSG2-VnKElingOTgCSdeIumfg/exec";
 
+export async function fetchSchedule() {
+
+  try {
+
+    const res = await fetch(
+      `${BACKEND_URL}?action=schedule`
+    );
+
+    const data = await res.json();
+
+    return Array.isArray(data) ? data : [];
+
+  } catch (err) {
+
+    console.error("Schedule fetch error:", err);
+
+    return [];
+  }
+}
