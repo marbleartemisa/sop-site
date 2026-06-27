c278ea1ba0b73f3c46d1fb49358a72975dd0d61c
marbleartemisa <artemisamarblesystem@gmail.com>
2026-06-27 11:28:31 -0400

    Implement createProject function to build project and tasks

--- dashboard/js/services/project-service.js
+++ dashboard/js/services/project-service.js
@@ -1,1 +1,32 @@
+import { buildProject } from "./project-builder.js";
+import { buildProjectTasks } from "./project-task-builder.js";
 
+export function createProject(state) {
+
+    //==========================
+    // Build Project
+    //==========================
+
+    const project =
+        buildProject(state);
+
+    //==========================
+    // Build Tasks
+    //==========================
+
+    const tasks =
+        buildProjectTasks(project);
+
+    //==========================
+    // Return complete package
+    //==========================
+
+    return {
+
+        project,
+
+        tasks
+
+    };
+
+}
