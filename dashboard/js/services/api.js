bb7ab8bb77ae5442fb77a7bf7fdc60824da1dcc3
marbleartemisa <artemisamarblesystem@gmail.com>
2026-06-29 12:10:26 -0400

    Implement POST function for API calls
    Added a POST function to handle API requests.

--- dashboard/js/services/api.js
+++ dashboard/js/services/api.js
@@ -1,2 +1,25 @@
 const API_URL =
     "https://script.google.com/macros/s/AKfycbzVLUTOzA0gGIfKml7kWBAWIjSE6g473aBDmFCg-cN2UzSG2-VnKElingOTgCSdeIumfg/exec";
+
+
+/*==================================================
+  POST
+==================================================*/
+
+export async function post(data) {
+
+  const response = await fetch(API_URL, {
+
+    method: "POST",
+
+    headers: {
+      "Content-Type": "application/json"
+    },
+
+    body: JSON.stringify(data)
+
+  });
+
+  return await response.json();
+
+}
