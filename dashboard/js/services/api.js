c5c8b3d4db9b9bf0122f86b3f9e70015462b03cb
marbleartemisa <artemisamarblesystem@gmail.com>
2026-06-29 14:06:37 -0400

    Modify post function for no-cors handling
    Updated post function to handle no-cors mode for API requests.

--- dashboard/js/services/api.js
+++ dashboard/js/services/api.js
@@ -1,15 +1,14 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbzVLUTOzA0gGIfKml7kWBAWIjSE6g473aBDmFCg-cN2UzSG2-VnKElingOTgCSdeIumfg/exec";
 
-
 /*==================================================
   POST
 ==================================================*/
-
 export async function post(data) {
 
   const response = await fetch(API_URL, {
 
     method: "POST",
+    mode: "no-cors", // 🔥 FIX TEMPORAL CORS
 
     headers: {
       "Content-Type": "application/json"
@@ -19,6 +18,5 @@ export async function post(data) {
 
   });
 
-  return await response.json();
-
+  return response; // no JSON disponible en no-cors
 }
